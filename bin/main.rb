73eaa20c4da7e76034e8d2be0f606a340774840f
KinezaEnaelle <kiris2024@gmail.com>
2020-11-25 13:46:01 +0100

    Add board display

--- bin/main.rb
+++ bin/main.rb
@@ -1,21 +1,33 @@
 #!/usr/bin/env ruby
- def create_player
-    puts "Please enter name of the first player"
-    player1 = gets.chomp()
-    puts "Welcome " + player1
-    puts "Please enter name of the second player"
-    player2 = gets.chomp()
-    puts "Welcome " + player2
+#  def create_player
+#     puts "Please enter name of the first player"
+#     player1 = gets.chomp()
+#     puts "Welcome " + player1
+#     puts "Please enter name of the second player"
+#     player2 = gets.chomp()
+#     puts "Welcome " + player2
 
- end
- create_player
+#  end
+#  create_player
 
- def player_key
-    puts " Choose your key"
-    key1 = gets.chomp()
-    puts "Thanks for choosing " + key1 
-    puts " Choose your key"
-    key2 = gets.chomp()
-    puts "Thanks for choosing " + key2
+#  def player_key
+#     puts " Choose your key"
+#     key1 = gets.chomp()
+#     puts "Thanks for choosing " + key1 
+#     puts " Choose your key"
+#     key2 = gets.chomp()
+#     puts "Thanks for choosing " + key2
+#  end
+#  player_key
+ 
+ def board_display
+    puts "Here is the Board"
+    board = Array.new(9,0)
+    i = 0
+    while i<board.length
+        print "#{board[i]} |" if i%3!=2
+        puts "#{board[i]} " if i%3==2
+        i+=1
  end
- player_key
+end
+ board_display
