795cd2fa37d08ecacac7ab611122342f6453f5e9
KinezaEnaelle <kiris2024@gmail.com>
2020-11-25 13:39:09 +0100

    Add player key input

--- bin/main.rb
+++ bin/main.rb
@@ -3,5 +3,19 @@
     puts "Please enter name of the first player"
     player1 = gets.chomp()
     puts "Welcome " + player1
+    puts "Please enter name of the second player"
+    player2 = gets.chomp()
+    puts "Welcome " + player2
+
  end
  create_player
+
+ def player_key
+    puts " Choose your key"
+    key1 = gets.chomp()
+    puts "Thanks for choosing " + key1 
+    puts " Choose your key"
+    key2 = gets.chomp()
+    puts "Thanks for choosing " + key2
+ end
+ player_key
