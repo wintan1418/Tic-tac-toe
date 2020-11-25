0eb94c5b47734d32f5f6e210e62570582e264c2a
KinezaEnaelle <kiris2024@gmail.com>
2020-11-25 12:39:16 +0100

    Add Player name input

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,12 @@
-#!/usr/bin/main ruby
-puts 'Hello World'
+#!/usr/bin/env ruby
+
+puts "Welcome to TIC TAC TOE Game"
+def create_player
+    puts "Please Enter the name of the first player"
+    player1 = gets.chomp()
+    puts "Welcome " + player1
+    puts "Please Enter the name of the second player"
+    player2 = gets.chomp()
+    puts "Welcome " + player2
+end
+create_player
