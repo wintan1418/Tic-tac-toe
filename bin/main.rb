46e17f2384e979d4fb2e197a1d6bfad4404d36d8
oluwadare juwon <wintan1418@gmail.com>
2020-11-25 10:49:36 +0100

    add basic user information,and defined methods

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,21 @@
 #!/usr/bin/main ruby
-puts 'Hello World'
+# frozen_string_literal: true
+
+class Player
+  attr_accessor = :name, :key, :player1, :player2
+  def initialize(name, key, player1, player2)
+    @name = name
+    @key = key
+    @player1 = Player.new(nil, 'x')
+    @player2 = Player.new(nil, 'o')
+  end
+
+  def create_player
+    puts 'input your name as the first player'
+    @player1.name = gets.chomp
+    puts 'input your name as the second player'
+    @player2.name = gets.chomp
+  end
+end
+p = Player.new
+p.create_player
