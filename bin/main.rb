c0e5daf033f6540e6b7b4358763bc57912ee71c2
oluwadare juwon <wintan1418@gmail.com>
2020-11-26 15:54:56 +0100

    add methods to signify validity of players move,and indicate player's turn

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+puts "welcome to Tic-tac-toe"
+
 def create_player
   puts 'Please enter name of the first player'
   player1 = gets.chomp
@@ -19,6 +21,30 @@ def player_key
 end
 player_key
 
+def player_switch
+  puts 'it is your turn to strike player1'
+  switch1 = gets.chomp
+  puts "thanks for playing #{switch1}"
+  puts 'it is your turn to strike player2'
+  switch2 = gets.chomp
+  puts "thanks for playing #{switch2}"
+
+end
+player_switch
+
+def move_validity
+  puts "player, make your move"
+  move = gets.chomp
+  board = Array.new(9, 0)
+  i = 0
+  while i < board.length
+    print "#{board[i]} |" if i % 3 != 2
+    puts "#{board[i]} " if i % 3 == 2
+    i += 1
+  end
+end
+move_validity
+
 def board_display
   puts 'Here is the Board'
   board = Array.new(9, 0)
@@ -29,4 +55,5 @@ def board_display
     i += 1
   end
 end
+  
 board_display
