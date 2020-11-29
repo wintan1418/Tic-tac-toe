6080cc24982f3dbbed918a1c9c605352b9b44ff7
KinezaEnaelle <kiris2024@gmail.com>
2020-11-29 22:46:58 +0100

    Add Game class

---
+++ lib/game.rb
@@ -0,0 +1,7 @@
+class Game
+    WINNING_COMBINATION = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]].freeze
+    attr_accessor :combination
+    def initialize
+        @combination = []
+    end
+end
