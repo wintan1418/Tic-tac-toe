0b23ce2b9c6f8044305385d48b29255857886a17
KinezaEnaelle <kiris2024@gmail.com>
2020-11-29 22:37:11 +0100

    Add Board class

---
+++ lib/board.rb
@@ -0,0 +1,17 @@
+class Board
+    attr_reader :key, :moves
+    def initialize(key,moves)
+        @key = Array.new[9]
+        @moves = []
+    end
+    def update_board(picked_cell,player)
+        @key[picked_cell] = player.key
+    end
+    def display_board
+        i = 0
+        while i<board.length
+            print"#{board[i]} |" if i%3!=2
+            puts"#{board[i]}" if i%3==2
+            i+=1
+    end
+end
