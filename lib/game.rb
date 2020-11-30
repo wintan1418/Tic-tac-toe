5191ea8eb95706d0e1c8def98b1f06d4553bc563
KinezaEnaelle <kiris2024@gmail.com>
2020-11-30 12:46:30 +0100

    Add display title method

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,11 @@
+require_relative '../lib/player.rb'
+require_relative '../lib/board.rb'
 class Game
     WINNING_COMBINATION = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]].freeze
-    attr_accessor :combination
+    attr_accessor :combination, :player1, :player2
     def initialize
         @combination = []
+        @player1 = Player.new(nil,"X")
+        @player2 = Player.new(nil,"O")
     end
 end
