1205e6dc451aa842a06ccaf98d6c1f1cadcd3b0f
oluwadare juwon <wintan1418@gmail.com>
2020-12-03 15:33:53 +0100

    make board update to every player input

--- bin/main.rb
+++ bin/main.rb
@@ -1,174 +1,102 @@
-# # !/usr/bin/env ruby
-
-# require_relative '../lib/player.rb'
-# require_relative '../lib/game.rb'
-# require_relative '../lib/board.rb'
-
-# def display_title
-# puts 'WELCOME TO TIC-TAC-TOE GAME'
-# puts '    _______ _          _______             _______          
-# (_______|_)        (_______)           (_______)         
-#     _    _  ____       _ _____  ____       _  ___  _____ 
-#     | |  | |/ ___)     | (____ |/ ___)     | |/ _ \| ___ |
-#     | |  | ( (___      | / ___ ( (___      | | |_| | ____|
-#     |_|  |_|\____)     |_\_____|\____)     |_|\___/|_____) '                                                         
-# end
-# display_title
-
-# def board_display
-#   puts 'Here is the Board'
-#   i = 0
-#   while i < $board.key.length
-#     print "#{$board.key[i]} |" if i % 3 != 2
-#     # puts '----------'
-#     puts "#{$board.key[i]} " if i % 3 == 2
-#     # puts '----------'
-#     i += 1
-#   end
-#   end
+# !/usr/bin/env ruby
+require_relative '../lib/player.rb'
+require_relative '../lib/game.rb'
+require_relative '../lib/board.rb'
 
 
-# # def board_choice_display
-# #   puts '      .-----.-----.-----.      .-----.-----.-----.'
-# #   puts "      |  1  |  2  |  3  |      |  #{$b_ar[0]}  |  #{$b_ar[1]}  |  #{$b_ar[2]}  |"
-# #   puts '      .-----+-----+-----.      .-----+-----+-----.'
-# #   puts "      |  4  |  5  |  6  | ==>> |  #{$b_ar[3]}  |  #{$b_ar[4]}  |  #{$b_ar[5]}  |"
-# #   puts '      .-----+-----+-----.      .-----+-----+-----.'
-# #   puts "      |  7  |  8  |  9  |      |  #{$b_ar[6]}  |  #{$b_ar[7]}  |  #{$b_ar[8]}  |"
-# #   puts '      .-----.-----.-----.      .-----.-----.-----.'
-# # end
+def display_title
+  puts 'WELCOME TO TIC-TAC-TOE GAME'
+  puts '    _______ _          _______             _______          
+  (_______|_)        (_______)           (_______)         
+      _    _  ____       _ _____  ____       _  ___  _____ 
+      | |  | |/ ___)     | (____ |/ ___)     | |/ _ \| ___ |
+      | |  | ( (___      | / ___ ( (___      | | |_| | ____|
+      |_|  |_|\____)     |_\_____|\____)     |_|\___/|_____) '                                                         
+end
+
+display_title
+
+$board = Board.new
+$player_1 = nil
+$player_2 = nil
+
+def board_display
+  puts 'Here is the Board'
+  i = 0
+  while i < $board.key.length
+    print "#{$board.key[i]} |" if i % 3 != 2
+    # puts '----------'
+    puts "#{$board.key[i]} " if i % 3 == 2
+    # puts '----------'
+    i += 1
+  end
+
+end
 
 
-# # def create_player
-# # puts 'Please enter name of the first player'
-# # player1 = gets.chomp
-# # puts "Welcome #{player1}"
-# # puts 'Please enter name of the second player'
-# # player2 = gets.chomp
-# # puts "Welcome #{player2}"
-# # end
-# # create_player
+def request_user_name(player_num)
+  puts "Enter your name player #{player_num}"
+    name = gets.chomp
+    name
+end
 
-# # def player_key
-# # puts 'player1 Choose your key'
-# # key1 = gets.chomp.downcase
-# # if key1 != 'x' && key1 != 'o'
-# #   puts 'Invalid key, choose x or o'
-# #   player_key
-# # end
-# # puts "player 1 you are assigned #{key1}"
-# # puts "player2 choose your key"
-# # key2 = gets.chomp.downcase
-# # if key2 != key1 && key2 == 'x' && key2 == 'o'
-# #   puts "player 2 you are assigned #{key2}"
-# # else
-# #     puts "invalid choice"
-# # end
-# # end
-# # player_key
-
-# # def player_key
-# #   puts 'player1 Choose your key'
-# #   key1 = gets.chomp
-# #   if key1 == 'x'
-# #     puts "Thanks for choosing #{key1}"
-# #     else
-# #     puts "wrong choice" 
-# #   end
-# #    puts 'player2 Choose your key'
-# #    key2 = gets.chomp
-# #    if key2 == 'o'
-# #     puts "Thanks for choosing #{key2}"
-# #   else
-# #     puts "wrong choice"
-# #   end
-# # end
-# # player_key
-
-# def request_user_name(player_num)
-#   puts "Enter your name player #{player_num}"
-#     name = gets.chomp
-#     name
-#   end
-  
-#   def get_player_tag
-#   puts 'Enter your tag x | o'
-#   tag = gets.chomp
-#   tag
-#   end
-#   def create_player 
-#   player_1_name = request_user_name(1)
-#   player_1_tag = get_player_tag
-#   player_2_name = request_user_name(2)
-#   player_2_tag = get_player_tag
-#   $player_1 = Player.new(player_1_name, player_1_tag)
-#   $player_2 = Player.new(player_2_name, player_2_tag)
-#   end
-#   puts create_player
+def get_player_tag
+  puts 'Enter your tag x | o'
+  tag = gets.chomp
+  tag
+end
 
 
-# # def player_switch
-# # puts 'it is your turn to strike player1'
-# # switch1 = gets.chomp
+def create_player 
+  player_1_name = request_user_name(1)
+  player_1_tag = get_player_tag
+  player_2_name = request_user_name(2)
+  player_2_tag = get_player_tag
+  $player_1 = Player.new(player_1_name, player_1_tag)
+  $player_2 = Player.new(player_2_name, player_2_tag)
+end
+create_player 
 
-# # board = Array.new(9,0)
-# # board_display
-# # puts "thanks for playing #{switch1}"
+def player_switch
+  puts 'it is your turn to strike player1'
+  switch1 = gets.chomp.to_i
+  $board.update_board(switch1 - 1, $player_1)
+  board_display
+  puts "thanks for playing #{switch1}"
+  puts 'it is your turn to strike player2'
+  switch2 = gets.chomp.to_i
+  $board.update_board(switch2 - 1, $player_2)
+  board_display
 
-# # puts 'it is your turn to strike player2'
-# # switch2 = gets.chomp
-# # board = Array.new(9,0)
-# # board_display
-# # puts "thanks for playing #{switch2}"
-# # end
+  puts "thanks for playing #{switch2}"
+end
 
-# # player_switch
-# def player_switch
-#   puts 'it is your turn to strike player1'
-#   switch1 = gets.chomp.to_i
-#   $board.update_board(switch1 - 1, $player_1)
-#   board_display
-#   puts "thanks for playing #{switch1}"
-#   puts 'it is your turn to strike player2'
-#   switch2 = gets.chomp.to_i
-#   $board.update_board(switch2 - 1, $player_2)
-#   board_display
-#   puts "thanks for playing #{switch2}"
-#   end
-#   player_switch
+player_switch
 
+def move_validity
+  puts 'player, make your move '
+  move = gets.chomp
+  board_display
+  puts "Good! your  #{move} is valid?"
+end
 
-# def move_validity
-# puts 'player, make your move '
-# move = gets.chomp
-# board = Array.new(9,0)
-# board_display(board)
+move_validity
 
-# puts "Good! your  #{move} is valid?"
-# end
-# move_validity
+def winning_move
+  puts 'please,make your move'
+  move = gets.chomp
+  board_display
+  puts "your #{move} as given you a win!"
+  puts 'opppps! play again'
+end
 
-# def winning_move
-# puts 'please,make your move'
-# move = gets.chomp
-# board = Array.new(9, 0)
-# board_display(board)
+winning_move
 
-# puts "your #{move} as given you a win!"
-# puts 'opppps! play again'
-# end
-# winning_move
+def draw_move
+  puts 'player1,make your move'
+  move = gets.chomp
+  board_display
+  puts "box is full.you both had a draw tie! #{move}"
+end
 
-# def draw_move
-# puts 'player1,make your move'
-# move = gets.chomp
-# board = Array.new(9, 0)
-# board_display(board)
-
-# puts "box is full.you both had a draw tie! #{move}"
-# end
-
-# draw_move
-
-
-
+draw_move
